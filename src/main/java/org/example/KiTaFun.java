bc8a98e0975d860ff7407232783b0e81d487e826
Markus Fauteck <mafa5093@bht-berlin.de>
2025-06-08 12:39:27 +0200

    new commit

---
+++ src/main/java/org/example/KiTaFun.java
@@ -0,0 +1,9 @@
+package org.example;
+public class KiTaFun {
+    public static void main(String[] args) {
+        System.out.println("5-Minuten-Kaffee-Kuchen:");
+        System.out.println("200 g Mehl, 1 Becher Joghurt …");
+        System.out.println("Oder lieber ein Witz?");
+        System.out.println("Warum essen Programmierer so gern Kekse? – Weil sie Cookies akzeptieren müssen!");
+    }
+}
